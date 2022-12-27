7ad5c1e80b62875da45a549acc0fe1c729e24e5a
Vitalii Matviienko <doktormc25@gmail.com>
2022-12-27 12:27:20 +0200

    alternative to switch

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -11,53 +11,39 @@ function Form({ onAddElement, changeActivity }) {
 
   const [nameError, setNameError] = useState(" ");
   const [surNameError, setSurNameError] = useState(" ");
-  const [phoneNumberError, setPhoneNumber] = useState(" ");
+  const [phoneNumberError, setPhoneNumberError] = useState(" ");
 
   const [formValid, setFormValid] = useState(false);
 
   useEffect(() => {
     if (nameError || surNameError || phoneNumberError) {
       setFormValid(false);
-      console.log(formValid);
     } else {
       setFormValid(true);
-      console.log(formValid);
     }
   }, [nameError, surNameError, phoneNumberError, formValid]);
 
+  const errorText = (name, empty) => {
+    const errors = {
+      name: (empty) =>
+        !empty ? setNameError("the name is not written!") : setNameError(""),
+      surname: (empty) =>
+        !empty
+          ? setSurNameError("the surname is not written!")
+          : setSurNameError(""),
+      phoneNumber: (empty) =>
+        !empty
+          ? setPhoneNumberError("the phone number is not written!")
+          : setPhoneNumberError(""),
+    };
+
+    return errors[name]?.(empty);
+  };
+
   const onInputChange = (e) => {
     const { value, name } = e.target;
-
     setContact({ ...contact, [name]: value });
-    if (!value) {
-      switch (name) {
-        case "name":
-          setNameError("the name is not written!");
-          break;
-        case "surname":
-          setSurNameError("the surname is not written!");
-          break;
-        case "phoneNumber":
-          setPhoneNumber("the phone nimber is not written!");
-          break;
-        default:
-          break;
-      }
-    } else {
-      switch (name) {
-        case "name":
-          setNameError("");
-          break;
-        case "surname":
-          setSurNameError("");
-          break;
-        case "phoneNumber":
-          setPhoneNumber("");
-          break;
-        default:
-          break;
-      }
-    }
+    errorText(name, value);
   };
 
   const onClick = (e) => {
